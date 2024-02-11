e0b7548896ab46e90131e887a7cbdcceaa86c0c1
Ja Tar <tarnogorskijan@gmail.com>
2024-02-11 22:50:35 +0100

    lepszy wygląd

--- Pokaz-Odp_eDesk.js
+++ Pokaz-Odp_eDesk.js
@@ -8,40 +8,39 @@
 // @match https://edesk.pearson.pl/*
 // @icon https://edesk.pearson.pl/favicon.ico
 // @grant none
-// @version 0.1.4
+// @version 0.1.5
 // ==/UserScript==
 
 setTimeout(() => {
-    console.log("Załadowano!");
-    
-    //Get the header container
-    var headerContainer = document.querySelector(".right-side-menu-container");
-    
-    //Create the run script button
-    var runBtn = document.createElement("button");
-    runBtn.innerHTML = "Pokaż ODPOWIEDZI";
-    runBtn.style.backgroundColor = "green";
-    runBtn.style.color = "white";
-    runBtn.style.padding = "10px";
-    runBtn.style.marginLeft = "10px";
-    
-    //Add the run script button to the header container
-    headerContainer.appendChild(runBtn);
-    
-    //Add click event listener to run script button
-    runBtn.addEventListener("click", function() {
+  console.log("Załadowano!");
+
+  //Get the header container
+  var headerContainer = document.querySelector(".right-side-menu-container");
+
+  //Create the run script button
+  var runBtn = document.createElement("button");
+  runBtn.innerHTML = "Pokaż ODPOWIEDZI";
+  runBtn.style.backgroundColor = "green";
+  runBtn.style.color = "white";
+  runBtn.style.padding = "10px";
+  runBtn.style.marginLeft = "10px";
+
+  //Add the run script button to the header container
+  headerContainer.appendChild(runBtn);
+
+  //Add click event listener to run script button
+  runBtn.addEventListener("click", function () {
     //Get the Show Answers button
-      const iframeDocument = document.getElementById('content-iframe').contentWindow.document;
-      var showAnswersBtn = iframeDocument.getElementById("Show_Answers1");
-    
+    const iframeDocument = document.getElementById('content-iframe').contentWindow.document;
+    var showAnswersBtn = iframeDocument.getElementById("Show_Answers1");
+
     //Check if Show Answers button exists
-      if (showAnswersBtn) {
-    //Change visibility to visible
-        showAnswersBtn.style.visibility = "visible";
-      }
-    });
-    }, 6000);
-    
-    
-    
-    
+    if (showAnswersBtn) {
+      //Change visibility to visible
+      showAnswersBtn.style.visibility = "visible";
+    }
+  });
+}, 6000);
+
+
+
